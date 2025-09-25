a0c9f50a8fd1e37fc9d0df7c238c991d40af1404
MarianaCediel <m.cediel1@uniandes.edu.co>
2025-09-25 18:46:59 -0500

    producto y order

--- provesi/orders/models.py
+++ provesi/orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from producto.models import Producto
 
 # Create your models here.
+class Orders(models.Model):
+    id = models.AutoField(primary_key=True)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, related_name="orders")
+    cantidad = models.PositiveIntegerField()
+    ubicacion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '%s %s' % (self.value, self.unit)
