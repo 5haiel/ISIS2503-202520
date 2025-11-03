60e1191456a2cd8640b49bc9e150041475f9d438
mpinedam <m.pinedam@uniandes.edu.co>
2025-11-03 10:58:02 -0500

    producto vuelve

---
+++ provesi/producto/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    sku = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '%s %s' % (self.nombre, self.sku)
