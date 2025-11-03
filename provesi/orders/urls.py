fe72aeb3da0cedb981f9bb8d69056b4f92933e71
mpinedam <m.pinedam@uniandes.edu.co>
2025-11-03 11:11:36 -0500

    creo que ahora si

--- provesi/orders/urls.py
+++ provesi/orders/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('orders/<int:order_id>/', views.order_detail_view, name='order-detail'),
-    path('orders/', views.OrdersListView.as_view(), name='orders-list'),
+    path('', views.OrdersListView.as_view(), name='orders-list'),          # GET /orders/
+    path('<int:order_id>/', views.order_detail_view, name='order-detail'), # GET /orders/1/
 ]
