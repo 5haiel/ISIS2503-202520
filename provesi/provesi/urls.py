4694e9020455b7ee4685fa5f4d49503c4de8e724
mpinedam <m.pinedam@uniandes.edu.co>
2025-11-05 21:21:19 -0500

    finalll

--- provesi/provesi/urls.py
+++ provesi/provesi/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
+def home(request):
+    return HttpResponse("<h1>Bienvenido a la API de Provesi 🚀</h1><p>Visita <a href='/orders/'>/orders/</a> para ver las órdenes.</p>")
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home),  # 👈 Página principal
     path('orders/', include('orders.urls')),
-    
 ]
