4e855d8fdd6b00a473aba809439511f914ba1b92
mpinedam <m.pinedam@uniandes.edu.co>
2025-11-05 20:09:07 -0500

    a ver

--- provesi/orders/views.py
+++ provesi/orders/views.py
@@ -1,17 +1,8 @@
-from django.shortcuts import render
-from django.http import JsonResponse, Http404
-from .repositories import get_order_with_product_and_location
-from rest_framework import generics
+
+from rest_framework import viewsets
 from .models import Orders
 from .serializers import OrdersSerializer
-# Create your views here.
 
-class OrdersListView(generics.ListAPIView):
+class OrdersViewSet(viewsets.ModelViewSet):
     queryset = Orders.objects.all()
     serializer_class = OrdersSerializer
-
-def order_detail_view(request, id: int):
-    data = get_order_with_product_and_location(id)
-    if data is None:
-        return JsonResponse({"detail": "Order not found"}, status=404)
-    return JsonResponse(data, status=200)
