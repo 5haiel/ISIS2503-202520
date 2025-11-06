aeeb7dc5f48e385884bfd5369fd1049fa811905a
mpinedam <m.pinedam@uniandes.edu.co>
2025-11-05 20:10:13 -0500

    eee fijo todo mal JAJAJA

--- provesi/orders/urls.py
+++ provesi/orders/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.OrdersListView.as_view(), name='orders-list'),
-    path('<int:id>/', views.order_detail_view, name='order-detail'),
-]
+from rest_framework.routers import DefaultRouter
+from .views import OrdersViewSet
+
+router = DefaultRouter()
+router.register(r'orders', OrdersViewSet, basename='orders')
+
+urlpatterns = router.urls
