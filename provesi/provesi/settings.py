427b3935eb0e87a4343303e9b205b1233ac19a94
mpinedam <m.pinedam@uniandes.edu.co>
2025-11-08 09:09:39 -0500

    ip db

--- provesi/provesi/settings.py
+++ provesi/provesi/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "NAME": "monitoring_db",
         "USER": "monitoring_user",
         "PASSWORD": "isis2503",
-        "HOST": "54.146.211.62",   # o el host del contenedor/servicio
+        "HOST": "54.227.54.5",   # o el host del contenedor/servicio
         "PORT": "5432",
     }
 }
