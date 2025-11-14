3f600253b6c93f24f0abb6031931126db894e978
Shaiel Jimenez <sm.jimenezp1@uniandes.edu.co>
2025-11-14 00:30:09 -0500

    update db ip

--- provesi/provesi/settings.py
+++ provesi/provesi/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "/login/auth0" 
 LOGIN_REDIRECT_URL = "/" 
-LOGOUT_REDIRECT_URL = "https://dev-my7jh7d04q10jkt6.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F3.92.27.87:8080" 
+LOGOUT_REDIRECT_URL = "https://dev-my7jh7d04q10jkt6.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F107.23.11.220:8080" 
 SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes 
 SOCIAL_AUTH_AUTH0_DOMAIN = 'dev-my7jh7d04q10jkt6.us.auth0.com'
 SOCIAL_AUTH_AUTH0_KEY = '3tpBWADn2Tjg9mkFXDHRfdA3QVDOQQDW' 
