815a62223871fa2e82edf1e1e3fbc69548426aee
Shaiel Jimenez <sm.jimenezp1@uniandes.edu.co>
2025-11-13 23:34:13 -0500

    scripts for db

--- provesi/producto/management/commands/import_productos.py
+++ provesi/producto/management/commands/import_productos.py
@@ -6,7 +6,7 @@ class Command(BaseCommand):
     help = "Import productos from CSV"
 
     def handle(self, *args, **kwargs):
-        path = "provesi/datos/productos.csv"
+        path = "datos/productos.csv"
 
         with open(path, newline='', encoding="utf-8") as f:
             reader = csv.DictReader(f)
